3e6662c4f7d68968d43d3e92444eae32912b9347
Alexey Pronin <alexey.pronin@uky.edu>
2016-11-20 13:33:25 -0800

    Corrected the info about the data sample.

--- R/input_specs.R
+++ R/input_specs.R
@@ -1,4 +1,4 @@
-#' Data specs
+#' Data specs example
 #'
 #' Data specs define the type of a variable for each column.
 #'
@@ -9,7 +9,7 @@
 #' \describe{
 #'      \item{\code{Variable}}{Variable names.}
 #'      \item{\code{Type}}{Describes how the column types must be specified:\cr\cr
-#'                  \code{character:} used to label samples(e.g. Sample ID);\cr
+#'                  \code{character:} used to label samples (e.g. Sample ID);\cr
 #'                  \code{numeric:} continuous numeric quantities (e.g. Weight, Age);\cr
 #'                  \code{categorical:} specifying categories of patients (e.g. Sex, CD8.ihc, Response);\cr
 #'                  \code{time:} specifying time to event (e.g. PFS, OS);\cr
