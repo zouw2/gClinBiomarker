32f4a0bb7a1d4d258fcdde74d9c00dce7d28eb95
Alexey Pronin <alexey.pronin@uky.edu>
2016-12-02 17:23:50 -0800

    Light update.

--- R/CreatePDF.R
+++ R/CreatePDF.R
@@ -1,5 +1,20 @@
+#' Create pdf file
+#' 
 #' @keywords internal
 #' @importFrom grDevices pdf dev.cur dev.off 
+#' @examples
+#' \dontrun{
+#' MainFunction <- function(..., pdfname = NULL, pdfparam = NULL) {
+#'    # Do something here
+#'    CreatePDF(pdfname, pdfparam)  
+#'    # Do plotting here 
+#'    CreatePDF(NULL) # Close the graphical device. 
+#'    # Do something here
+#' }
+#'
+#' MainFunction(..., pdfname = "myplot.pdf", pdfparam = list(width=10, height=5))
+#' }
+#' @export
 
 CreatePDF <- function(pdfname, pdfparam) {
     if (dev.cur() > 1) {
