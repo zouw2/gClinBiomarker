58a3a4d65aa4de5c58bf7cbc2a503ecefefb67fa
Alexey Pronin <pronin.alexey@gene.com>
2017-03-28 08:44:32 -0700

    Remove creating two plots.

--- R/PlotDens.R
+++ R/PlotDens.R
@@ -6,13 +6,13 @@
 #' 
 #' @param data Input data frame. Rows are patients and columns are variables (e.g. demographics variables, time to event variables, 
 #' biomarker variables, treatment indicator, etc.). One patient per row. 
-#' @param var Name of a variable (e.g. a biomarker variable). It can be a list of variables. Should be in colnames of \code{data}.  
+#' @param var Name of a variable (e.g. a biomarker variable). Should be in colnames of \code{data}.  
 #' @param log2 If TRUE, computes binary (i.e. base 2) logarithm. Default is FALSE.
 #' @param col The color of the line segments. Default is blue.
 #' @param text.font Legend text font size. Default is 3.
-#' @param main The main title. Default is \code{"Distribution of"}. A variable name will be added at the end of the title.
+#' @param main The main title. Default is \code{paste("Distribution of", var)}. A variable name will be added at the end of the title.
 #' @param add.num The constant to add to all values. Helps to avoid applying log transformation on 0 or negative values. Default is 0.
-#' @param xlab X axis label. Default is \code{ifelse(log2 == TRUE, "(log2 scale)", "")}. A variable name will be added at the beginning of the label.
+#' @param xlab X axis label. Default is \code{paste(var, ifelse(log2 == TRUE, "(log2 scale)", ""))}. A variable name will be added at the beginning of the label.
 #' @param pdf.name Name of output pdf file. If it's NULL (default), the plots will be displayed but not saved as pdf.
 #' @param pdf.param A list of parameters that define pdf graphics device. See \code{\link{pdf}}. Default is \code{list(width=6, height=4.5)}. 
 #' @param par.param A list of parameters that define graphcial parameters. See \code{\link{par}}. Default is \code{list(mar=c(4,4,3,2))}.
@@ -22,8 +22,6 @@
 #' @examples
 #' data(input)
 #' PlotDens(data=input, var="KRAS.exprs", log2=TRUE)
-#' PlotDens(data=input, var=list("KRAS.exprs", "OS"), log2=TRUE,
-#'          par.param=list(mar=c(4,4,3,2), mfrow=c(1,2), cex=0.75))
 #' 
 #' @export
 
@@ -32,9 +30,9 @@ PlotDens <- function(data,
                      log2=FALSE,
                      col="blue",
                      text.font=3,
-                     main="Distribution of",
+                     main=paste("Distribution of", var),
                      add.num=0,
-                     xlab=ifelse(log2 == TRUE, "(log2 scale)", ""),
+                     xlab=paste(var, ifelse(log2 == TRUE, "(log2 scale)", "")),
                      pdf.name=NULL,
                      pdf.param=list(width=6, height=4.5),
                      par.param=list(mar=c(4,4,3,2))) {
@@ -54,34 +52,31 @@ PlotDens <- function(data,
     
     PlotParam(pdf.name, pdf.param, par.param)  
     
-    # Create a plot for each variable
-    for (i in 1:length(var)) {
-        V <- data[, var[[i]]] + add.num
-        
-        if (log2 == TRUE) {
-            if (any(data[, var[[i]]] <= 0, na.rm=T)) {
-                stop(paste(var, " contains values less than or equal 0! 
+    V <- data[, var] + add.num
+    
+    if (log2 == TRUE) {
+        if (any(data[, var] <= 0, na.rm=T)) {
+            stop(paste(var, " contains values less than or equal 0! 
                        No log transformation possible! You may set add.num to add a constant to all values."))
-            }
-            V <- log2(V)
         }
-        
-        sd.v <- round(sd(V, na.rm=TRUE), 2)
-        s.v <- summary(V)
-        leg.text <- paste(c("Mean", "SD", "Median", "Range"),
-                          c(round(s.v, 2)["Mean"], 
-                            sd.v, 
-                            round(s.v, 2)["Median"],
-                            paste("(", round(s.v["Min."], 2), " - ", round(s.v["Max."], 2), ")", sep="")),
-                          sep=": ")
-        
-        y2 <- max(density(V, na.rm=T)$y, na.rm=TRUE)
-        hist(V, prob=T, main=paste(main, var[[i]], sep=" "), xlab=paste(var[[i]], xlab, sep=" "), col="grey", ylim=c(0, y2))
-        lines(density(V, na.rm=T), col=col, lwd=2)
-        legend("topright", leg.text, bty="n", text.font=text.font)
-        mtext(side=3, line=0, paste("N=", sum(!is.na(V))))
-        box()
+        V <- log2(V)
     }
+    
+    sd.v <- round(sd(V, na.rm=TRUE), 2)
+    s.v <- summary(V)
+    leg.text <- paste(c("Mean", "SD", "Median", "Range"),
+                      c(round(s.v, 2)["Mean"], 
+                        sd.v, 
+                        round(s.v, 2)["Median"],
+                        paste("(", round(s.v["Min."], 2), " - ", round(s.v["Max."], 2), ")", sep="")),
+                      sep=": ")
+    
+    y2 <- max(density(V, na.rm=T)$y, na.rm=TRUE)
+    hist(V, prob=T, main=main, xlab=xlab, col="grey", ylim=c(0, y2))
+    lines(density(V, na.rm=T), col=col, lwd=2)
+    legend("topright", leg.text, bty="n", text.font=text.font)
+    mtext(side=3, line=0, paste("N=", sum(!is.na(V))))
+    box()
     
     PlotParam()
 }
