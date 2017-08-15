159610a42e72cf76053d039cb23496abb08e9373
Ning Leng <lengn@gene.com>
2017-08-15 14:19:10 -0700

    PlotRspbar typo
    in mar

--- R/PlotRspBar.R
+++ R/PlotRspBar.R
@@ -188,7 +188,7 @@ PlotRspBar <- function(data, outcome.var,
     # Define par parameters
     if(is.null(par.param$mar)) {
         p1 <- max(5,.3*max.bi.txt) 
-        p2 <- max(5,.5*max.bi.txt)
+        p2 <- max(5,.5*max.name.txt)
         if(horiz)par.param$mar <- c(5,p1,4,p2)
         if(!horiz) par.param$mar <- c(p1,4,p2,5)
     }
