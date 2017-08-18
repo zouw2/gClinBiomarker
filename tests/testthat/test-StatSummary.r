4682eedaca22dc13fc42263653099f4f5435c557
Alexey Pronin <pronin.alexey@gene.com>
2017-08-18 09:50:20 -0700

    light update

--- tests/testthat/test-StatSummary.r
+++ tests/testthat/test-StatSummary.r
@@ -1,7 +1,9 @@
 context("StatSummary")
 
 test_that("Confidence Interval", {
+    data(input)
     res <- StatSummary(outcome.var = input$OS, treatment.var = input$Arm, placebo.code = "CTRL", active.code = "TRT", outcome.class = "continuous")
+
     expect_that(res["Lower"] <= res["Upper"], is_true())
     expect_that(res["P"] <= 1 & res["P"] >= 0, is_true())
     expect_that(res["Effect.Size"] >= res["Lower"] & res["Effect.Size"] <= res["Upper"], is_true())
