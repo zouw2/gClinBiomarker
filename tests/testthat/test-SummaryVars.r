fc451cd8f3526eac56b98969064c8a8e16bf5ee8
Alexey Pronin <pronin.alexey@gene.com>
2017-08-18 10:09:20 -0700

    add new tests

--- tests/testthat/test-SummaryVars.r
+++ tests/testthat/test-SummaryVars.r
@@ -5,4 +5,10 @@ test_that("Size of data", {
     res <- SummaryVars(data=input, trt='Arm', subgroup='BEP', var=c('Age','Sex'), var.class=c("numeric","categorical"))
 
     expect_equal(as.numeric(res["N", "All(CTRL)"]) + as.numeric(res["N", "All(TRT)"]), nrow(input))
+    expect_equal(as.numeric(res["Total", "All(CTRL)"]) + as.numeric(res["Total", "All(TRT)"]), nrow(input))
+
+    expect_equal(as.numeric(res["Total", "All(CTRL)"]), as.numeric(res["N", "All(CTRL)"]))
+    expect_equal(as.numeric(res["Total", "BEP(CTRL)"]), as.numeric(res["N", "BEP(CTRL)"]))
+    expect_equal(as.numeric(res["Total", "All(TRT)"]), as.numeric(res["N", "All(TRT)"]))
+    expect_equal(as.numeric(res["Total", "BEP(TRT)"]), as.numeric(res["N", "BEP(TRT)"]))
 })
