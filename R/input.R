28c2748fe9b20df28868d58e1eb5f9053947388f
Ning Leng <lengn@gene.com>
2017-09-14 11:41:02 -0700

    Revert "update input.specs with new variable"
    This reverts commit f318cab8131e6298a92eb1a9f11169e614c298b9.

--- R/input.R
+++ R/input.R
@@ -5,10 +5,10 @@
 #' @author Alexey Pronin \email{pronin.alexey@gene.com}, Ning Leng \email{leng.ning@gene.com}
 #'
 #' @name input
-#'
+#' 
 #' @usage data(input)
 #' input
-#'
-#' @format A data frame with 550 rows and 19 variables.
+#' 
+#' @format A data frame with 550 rows and 18 variables.
 #'
 NULL
